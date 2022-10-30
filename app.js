acdff389a29d443fe4cfe45b0b3f0891249ac554
dhaneshsaini <sainidhanesh58@gmail.com>
2022-10-30 17:52:05 +0530

    worldclock&css

--- app.js
+++ app.js
@@ -1,23 +1,45 @@
 const fs = require('fs')
 const path = require('path')
 
-const testArr = fs.readdirSync(path.join(__dirname + '/_length'))
+const testArr = fs.readdirSync(path.join(__dirname + '/_worldclock'))
 
 testArr.forEach(e => {
-  fs.appendFileSync('_length/' + e, `
+  fs.appendFileSync('_worldclock/' + e, `
   **Related Keywords**
 
   <ul class='relatedKeyword'>
-    <li>unit converter length</li>
-    <li>length conversion calculator</li>
-    <li>length conversion table</li>
-    <li>dimension converter</li>
-    <li>distance conversion table</li>
-    <li>converter size</li>
-    <li>1 square feet in cm length and width</li>
-    <li>nanometer to angstrom</li>
-    <li>dimension converter</li>
-    <li>length conversion calculator</li>
+    <li>worldclock</li>
+    <li>now time</li>
+    <li>timezone</li>
+    <li>est time now</li>
+    <li>utc time now</li>
+    <li>pst time now</li>
+    <li>cst time now</li>
+    <li>gmt time now</li>
+    <li>cet time now</li>
+    <li>sunset time today</li>
+    <li>eastern time zone</li>
+    <li>central time zone</li>
+    <li>ist time now</li>
+    <li>pacific time now</li>
+    <li>edt time now</li>
+    <li>est now</li>
+    <li>pdt time now</li>
+    <li>pst time right now</li>
+    <li>time right now</li>
+    <li>pacific time zone</li>
+    <li>gmt time zone</li>
+	  <li>central time now</li>
+	  <li>timezones</li>
+	  <li>mst time now</li>
+	  <li>my time zone</li>
+	  <li>ist now</li>
+	  <li>pst now</li>
+	  <li>bst time now</li>
+	  <li>cdt time now</li>
+	  <li>sunrise time today</li>
+	  <li>aest time now</li>
+    <li>world clock time</li>
   </ul>
   `)
 })
